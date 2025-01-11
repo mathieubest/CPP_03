bb9be8e9edf9c215607b7b45a89e347b2cf367bc
Mathieu Best <mbest@bess-f2r4s7.clusters.42paris.fr>
2025-01-11 14:27:23 +0100

    Cleaned and prepared for push

--- ex00/includes/ClapTrap.hpp
+++ ex00/includes/ClapTrap.hpp
@@ -5,14 +5,19 @@
 
 class ClapTrap {
     private:
-        std::string Name;
-        int hitPoint;
-        int Energy;
-        int attackDamage;
+        std::string _Name;
+        int _hitPoint;
+        int _Energy;
+        int _attackDamage;
 
     public:
+        // Constructors
         ClapTrap(const std::string& name);
+        
+        // Deconstructors
         ~ClapTrap();
+
+        // Public Methods
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,20 +1,23 @@
 #include "../includes/ClapTrap.hpp"
+#include "../includes/ColorCodes.hpp"
 
 int main() {
+    std::cout << BOLD_BLUE << "-- Constructing --" << RESET << std::endl;
     ClapTrap a("Mouss");
     ClapTrap b("Broski");
 
+    std::cout << BOLD_BLUE << "-- Testing --" << RESET << std::endl;
     b.takeDamage(10);
     b.attack("Sam");
     b.beRepaired(5);
-    std::cout << std::endl;
     
     a.takeDamage(5);
     a.beRepaired(3);
     for (int i = 1; i <= 15; i++) {
         std::cout << "[" << i << "] ";
-        a.attack("El Fuente");
+        a.attack("Boogie");
     }
     a.beRepaired(5);
+    std::cout << BOLD_BLUE << "-- Deconstructing --" << RESET << std::endl;
     return (0);
 }
