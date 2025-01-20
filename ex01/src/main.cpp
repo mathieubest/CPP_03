cd053d732603e592136567219a3cf66c5dc6c8e0
mathieubest <99485247+mathieubest@users.noreply.github.com>
2025-01-20 18:35:08 +0100

    Cleaned and prepared for push

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,16 +1,21 @@
 #include "../includes/ScavTrap.hpp"
+#include "../includes/ColorCodes.hpp"
 
 int main() {
-    ScavTrap a("Mista");
+    std::cout << BOLD_BLUE << "-- Constructing --" << RESET << std::endl;
+    ScavTrap def;
+    ScavTrap a("Tata");
 
-    a.attack("Broski");
+    std::cout << BOLD_BLUE << "-- Testing --" << RESET << std::endl;
+    a.attack("Toto");
     a.takeDamage(80);
-    a.attack("Broski");
+    a.attack("Titi");
     a.guardGate();
     a.beRepaired(20);
     a.takeDamage(39);
     a.beRepaired(9);
     a.takeDamage(10);
     a.beRepaired(50);
+    std::cout << BOLD_BLUE << "-- Deconstructing --" << RESET << std::endl;
     return (0);
 }
